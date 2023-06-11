2cdbbb9dc887c1630df0a0047c991ebf049149b9
marianoar <arpon.devar@gmail.com>
2023-06-11 19:49:40 -0300

    Factory patterns first part

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.FactoryPattern;
 using DesignPatterns.Singleton;
 
 Console.WriteLine("Hello, World!");
@@ -14,3 +15,16 @@ var b = Singleton.Instance;
 
 //verifico si es el mismo objeto
 Console.WriteLine(a == b);
+
+Console.WriteLine(" ---- Factory Patterns ---- ");
+
+// no puedo crear un objeto de la clase abstracta, pero si de sus hijos
+SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+
+SaleFactory internetSaleFactory = new InternetSaleFactory(5);
+
+ISale saleA = storeSaleFactory.GetSale();
+saleA.Sell(100);
+
+ISale saleB = internetSaleFactory.GetSale();
+saleB.Sell(100);
