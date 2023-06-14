2f0a29d09d8d08dff286b0439b4d889b1421bf1d
marianoar <arpon.devar@gmail.com>
2023-06-14 19:51:26 -0300

    factory method second part

---
+++ DesignPatterns/DesignPatternsASP/Controllers/ProductDetailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternsASP.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.20m);
+
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.20m, 33);
+
+            //products
+            var LocalEarn = localEarnFactory.GetEarn();
+            var foreignEarn = foreignEarnFactory.GetEarn();
+
+            //total
+            ViewBag.totalLocal = total + LocalEarn.Earn(total);
+            ViewBag.totalForeign = total + foreignEarn.Earn(total);
+            return View();
+        }
+    }
+}
---
+++ DesignPatterns/Tools/Earn/EarnFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Earn
+{
+    //pdria haber echo una interface tambien
+    public abstract class EarnFactory
+    {
+        public abstract IEarn GetEarn();
+    }
+}
